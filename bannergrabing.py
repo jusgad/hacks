030f2873d94f0b0efea9e731e5281f053b9daad8
Sebas-Adarve <jusga.adarve@gmail.com>
2023-12-11 12:03:27 -0500

    primer comentario

---
+++ bannergrabing.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+def banner(ip, puerto):
+    s = socket.socket() 
+    s.connect((ip, puerto))
+    print(str(s.recv(1024)))
+
+def main():
+    ip = input("Ingresa la ip del objetivo: ")
+    port = int(input("Ingresa el puerto: "))
+    banner(ip, port)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
